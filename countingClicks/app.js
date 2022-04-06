eb52e99b75ee6f857bf99c48bbd8372438aee930
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-04-06 17:34:41 +0200

    counting

---
+++ countingClicks/app.js
@@ -0,0 +1,37 @@
+class App extends React.Component {
+
+  state = {
+    count: 0,
+    result: 0
+  }
+
+  subtract = () => {
+    this.setState(prevState => ({
+      count: prevState.count+1,
+      result: prevState.result - 1
+    }))
+  }
+
+  increase = () =>{
+    this.setState(prevState => ({
+      count: prevState.count+1,
+      result: prevState.result +1
+    }))
+
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={this.subtract}>-1</button>
+        <button>RESET</button>
+        <button onClick={this.increase}>+1</button>
+        <h1>Liczba kliknięć: {this.state.count}</h1>
+        <h2>Wynik: {this.state.result}</h2>
+      </React.Fragment>
+
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
