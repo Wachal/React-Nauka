8f56cf7b6c1b57a9976a4dc09d0542a5b1fc92f6
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-04-11 19:02:09 +0200

    add form and button

--- checkbox/app.js
+++ checkbox/app.js
@@ -6,29 +6,49 @@ class CheckboxAge extends React.Component {
 
   state = {
     isConfirmed: false,
+    isFormShow: true,
+    isFormSubmitted: false
   }
 
   handleChange = () => {
     this.setState({
-      isConfirmed: !this.state.isConfirmed
+      isConfirmed: !this.state.isConfirmed,
+      isFormShow: !this.state.isFormShow,
+      isFormSubmitted: false
     })
   }
 
-  displayMessage = () => {
-    if (this.state.isConfirmed) {
-      return <PositiveMessage />
-    } else {
-      return <NeativeMessage />
+  handleChangeFormSubmit = (e) => {
+    e.preventDefault()
+    if(!this.state.isFormSubmitted){
+      this.setState({
+        isFormSubmitted: true
+      })
     }
+  }
+
+  displayMessage = () => {
+    if (this.state.isFormSubmitted) {
+      if (this.state.isConfirmed) {
+        return <PositiveMessage />
+      } else {
+        return <NeativeMessage />
+      }
+    } else { return null }
   }
 
   render() {
     return (
       <>
         <h1>Kup bilet na film</h1>
-        <input type="checkbox" id="age" onChange={this.handleChange} checked={this.state.isConfirmed} />
-        <label htmlFor="age">Mam powyzej 18 lat</label>
-        {this.displayMessage()}
+        <form onSubmit={this.handleChangeFormSubmit}>
+          <input type="checkbox" id="age" onChange={this.handleChange} checked={this.state.isConfirmed} />
+          <label htmlFor="age">Mam powyzej 18 lat</label>
+          {this.displayMessage()}
+          <p hidden={this.state.isFormShow}>Mozesz kupic bilet</p>
+          <br />
+          <button>Kup bilet</button>
+        </form>
       </>
     )
   }
