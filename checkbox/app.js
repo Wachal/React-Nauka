14c9901564b2d06dffc4a72c3dd3e76bc6d43857
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-04-12 11:17:55 +0200

    start shopping cart

--- checkbox/app.js
+++ checkbox/app.js
@@ -1,26 +1,28 @@
-const PositiveMessage = () => <p>Mozesz obejrzec</p>
 
-const NeativeMessage = () => <p> nie mozesz obejrzec</p>
+const ValidationMessage = (props) => {
+  const { thx } = props
+  return (
+    <p>{props.txt}</p>
+  )
+}
 
 class CheckboxAge extends React.Component {
 
   state = {
     isConfirmed: false,
-    isFormShow: true,
     isFormSubmitted: false
   }
 
   handleChange = () => {
     this.setState({
       isConfirmed: !this.state.isConfirmed,
-      isFormShow: !this.state.isFormShow,
       isFormSubmitted: false
     })
   }
 
   handleChangeFormSubmit = (e) => {
     e.preventDefault()
-    if(!this.state.isFormSubmitted){
+    if (!this.state.isFormSubmitted) {
       this.setState({
         isFormSubmitted: true
       })
@@ -30,22 +32,23 @@ class CheckboxAge extends React.Component {
   displayMessage = () => {
     if (this.state.isFormSubmitted) {
       if (this.state.isConfirmed) {
-        return <PositiveMessage />
+        return <ValidationMessage txt="Mozesz oglądac" />
       } else {
-        return <NeativeMessage />
+        return <ValidationMessage txt="nie mozesz ogladac" />
       }
     } else { return null }
   }
 
   render() {
+    const { isConfirmed } = this.state
+
     return (
       <>
         <h1>Kup bilet na film</h1>
         <form onSubmit={this.handleChangeFormSubmit}>
-          <input type="checkbox" id="age" onChange={this.handleChange} checked={this.state.isConfirmed} />
+          <input type="checkbox" id="age" onChange={this.handleChange} checked={isConfirmed} />
           <label htmlFor="age">Mam powyzej 18 lat</label>
           {this.displayMessage()}
-          <p hidden={this.state.isFormShow}>Mozesz kupic bilet</p>
           <br />
           <button>Kup bilet</button>
         </form>
