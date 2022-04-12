6c16de9716a286370db8780f804ee81af6d82c30
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-04-12 14:31:12 +0200

    add filter

--- arrays/app.js
+++ arrays/app.js
@@ -1,14 +1,78 @@
+const data = {
+  users: [
+    {
+      id: 1,
+      age: 30,
+      name: "Arek",
+      sex: "male"
+    },
+    {
+      id: 2,
+      age: 20,
+      name: "Kuba",
+      sex: "male"
+    },
+    {
+      id: 3,
+      age: 35,
+      name: "Zosia",
+      sex: "female"
+    },
+  ]
+}
+
+const Item = ({ user }) => (
+  <div className="userInfo">
+    <h1>{`Imię ${user.name}`}</h1>
+    <p>Informajce o uzytkowniku:</p>
+    <p>{`Wiek ${user.age}`}</p>
+    <p>{`płeć ${user.sex}`}</p>
+    <br />
+  </div>
+)
+
 class ListItems extends React.Component {
   state = {
-    items: ["gruszka", "ziemniak", "banan"]
+    select: "all",
   }
+
+  usersList = () => {
+    let users = this.props.data.users
+
+    switch (this.state.select) {
+      case "all":
+        return users.map(user => <Item user={user} key={user.id} />)
+
+      case "female":
+        users = users.filter(user => user.sex === "female")
+        return users.map(user => <Item user={user} key={user.id} />)
+
+      case "male":
+        users = users.filter(user => user.sex === "male")
+        return users.map(user => <Item user={user} key={user.id} />)
+
+      default:
+        return "niepoprawne dane"
+    }
+  }
+
+  handleUsersFilter = (option) => {
+    this.setState({
+      select: option
+    })
+  }
+
   render() {
+
     return (
-      <ul>
-        {this.state.items.map(item => <li key={item}>{`owoc: ${item}`}</li> )}
-      </ul>
+      <div>
+        <button onClick={this.handleUsersFilter.bind(this, "all")}>wszyscy </button>
+        <button onClick={this.handleUsersFilter.bind(this, "female")}>kobiety</button>
+        <button onClick={this.handleUsersFilter.bind(this, "male")}>mezczyzni</button>
+        {this.usersList()}
+      </div>
     )
   }
 }
 
-ReactDOM.render(<ListItems />, document.getElementById('root'))
+ReactDOM.render(<ListItems data={data} />, document.getElementById('root'))
