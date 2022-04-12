00bdfb67764b7acbb09c385bb1de9ce42112ad25
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-04-12 12:08:32 +0200

    end

--- shoppingCart/app.js
+++ shoppingCart/app.js
@@ -1,3 +1,33 @@
+class App extends React.Component {
 
+  state = {
+    avaibleProducts: 7,
+    shoppingCart: 0
+  }
+
+  handleRemoveFromCart = () =>{
+    this.setState({
+      shoppingCart: this.state.shoppingCart - 1
+    })
+  }
+
+  handleAddToCart = () =>{
+    this.setState({
+      shoppingCart: this.state.shoppingCart + 1
+    })
+  }
+
+  render() {
+    const { shoppingCart, avaibleProducts } = this.state
+
+    return (
+      <>
+        <button onClick={this.handleRemoveFromCart} disabled={shoppingCart ? false : true}>-</button>
+        <span> {shoppingCart} </span>
+        <button onClick={this.handleAddToCart} disabled={shoppingCart >= avaibleProducts ? true : false}>+</button>
+      </>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
