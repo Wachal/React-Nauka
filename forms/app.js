93ce9704acf299676e1b31ad050200e9bd13498d
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-04-22 12:56:58 +0200

    start forms

---
+++ forms/app.js
@@ -0,0 +1,25 @@
+class Form extends React.Component {
+  
+
+  render() {
+    return (
+      <form>
+        <label>
+          Podaj imię:
+          <input type="text" name="name"/>
+        </label>
+      <br/>
+      <label>
+        Podaj mail:
+        <input type="email" name="email"/>
+      </label>
+      <br/>
+      <button>zapisz się</button>
+      </form>
+
+    )
+  }
+}
+
+
+ReactDOM.render(<Form />, document.getElementById('root'))
