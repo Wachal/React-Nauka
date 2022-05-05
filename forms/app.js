d4e05e78ab4cb4f99adcf60ebf941462c7dc2409
Tymoteusz <61839202+Wachal@users.noreply.github.com>
2022-05-05 15:39:08 +0200

    forms

--- forms/app.js
+++ forms/app.js
@@ -1,22 +1,70 @@
 class Form extends React.Component {
-  
+  state = {
+    city: "Londyn",
+    text: "",
+    isLiked: true,
+    number: "2",
+  }
+
+  handleChange = e =>{
+    if(e.target.type === "checkbox"){
+      this.setState({
+        [e.target.name]: e.target.checked
+      })
+    }else{
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+  }
 
   render() {
     return (
-      <form>
+      <div>
         <label>
-          Podaj imię:
-          <input type="text" name="name"/>
+          Podaj miasto
+          <input 
+            name="city"
+            value={this.state.city} 
+            onChange={this.handleChange} 
+            type="text" 
+          />
         </label>
-      <br/>
-      <label>
-        Podaj mail:
-        <input type="email" name="email"/>
-      </label>
-      <br/>
-      <button>zapiszzz się</button>
-      </form>
-
+        <br />
+        <label>
+          Napisz cos o miescie
+          <textarea 
+            name="text"
+            value={this.state.text} 
+            onChange={this.handleChange}
+          ></textarea>
+        </label>
+        <br />
+        <label>
+          Czy lubisz to miasto?
+          <input 
+            name="isLiked"
+            type="checkbox" 
+            checked={this.state.isLiked} 
+            onChange={this.handleChange}
+          ></input>
+        </label>
+        <br/>
+        <label>
+          ile raz tam byles
+          <select 
+            name="number" 
+            value={this.state.number} 
+            onChange={this.handleChange}
+          >
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="more">więcej</option>
+          </select>
+        </label>
+      </div>
     )
   }
 }
