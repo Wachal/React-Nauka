e770f5367b7ad5bab14cd852abcfeed1b5e2a594
Tymoteusz <61839202+Wachal@users.noreply.github.com>
2022-05-21 15:34:45 +0200

    Create AddTask.js

---
+++ ToDoApp/AddTask.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+class AddTask extends React.Component {
+	state = {
+
+	}
+
+	render() {
+		return (
+			<div>
+				add TaskList
+			</div>
+		)
+	}
+}
+
+export default AddTask
