103f02fac1d2ee1b7be90043405f689329201aea
Tymoteusz <61839202+Wachal@users.noreply.github.com>
2022-05-23 17:29:46 +0200

    Update Task.js

--- ToDoApp/Task.js
+++ ToDoApp/Task.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+const Task = (props) => {
+
+  const  {text, date, id} = props.task
+  return (
+    <div className="taskClass">
+      <p>
+        <strong>{text}</strong> - do <span>{date}</span>
+      </p>
+      <div>
+        <button onClick={()=>props.change(id)}>Zostało zrobione</button>
+        <button onClick={()=>props.delete(id)}>X</button>
+      </div>
+    </div>
+  )
+}
+
+export default Task;
