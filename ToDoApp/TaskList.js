fb25787b685f35f50ee9bf7d8b00c9ea006cb366
Tymoteusz <61839202+Wachal@users.noreply.github.com>
2022-05-25 16:40:57 +0200

    Update TaskList.js

--- ToDoApp/TaskList.js
+++ ToDoApp/TaskList.js
@@ -3,15 +3,22 @@ import Task from './Task';
 
 const TaskList = (props) => {
 
-  const tasks = props.tasks.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change}/>)
+  const active = props.tasks.filter(task => task.active)
+  const done = props.tasks.filter(task => !task.active)
+  const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change}/>)
+  const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change}/>)
+
   return (
     <div>
       <div className="active">
         <h1> zadania do zrobienia</h1>
-        {tasks}
+        {activeTasks.length ? activeTasks : <p>brak zadan do zrobienia</p>}
       </div>
       <div className="active">
-        <h3> zadania zrobione (0)</h3>
+        <h3> zadania zrobione <em>{done.length}</em></h3>
+        {done.length>4 && 
+        <span>wyświetlonych jest 5 ostatnich zadań</span>}
+        {doneTasks.slice(0, 5)}
       </div>
     </div>
   )
