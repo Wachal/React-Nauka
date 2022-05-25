db00b852c5ee621abf77565b6a54ce9b26bc9da6
Tymoteusz <61839202+Wachal@users.noreply.github.com>
2022-05-25 16:40:42 +0200

    Update Task.js

--- ToDoApp/Task.js
+++ ToDoApp/Task.js
@@ -2,18 +2,40 @@ import React from "react";
 
 const Task = (props) => {
 
-  const  {text, date, id} = props.task
-  return (
-    <div className="taskClass">
-      <p>
-        <strong>{text}</strong> - do <span>{date}</span>
-      </p>
-      <div>
-        <button onClick={()=>props.change(id)}>Zostało zrobione</button>
-        <button onClick={()=>props.delete(id)}>X</button>
+  const style = {
+    color: 'red'
+  }
+
+  const { text, date, id, active, important, finishDate } = props.task
+
+  if (active) {
+    return (
+      <div className="taskClass">
+        <p>
+          <strong style={important ? style : null}>{text}</strong> - do <span>{date}</span>
+        </p>
+        <div>
+          <button onClick={() => props.change(id)}>Zostało zrobione</button>
+          <button onClick={() => props.delete(id)}>X</button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }else {
+
+    const finish = new Date(finishDate).toLocaleString()
+    return(
+      <div className="taskClass">
+        <p>
+          <strong>{text}</strong> - do <span>(zrobić do: {date})</span>
+        </p>
+        <br/>
+        <p>- potwierdzenie wykonania<span> {finish}</span></p>
+        <div>
+          <button onClick={() => props.delete(id)}>X</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Task;
